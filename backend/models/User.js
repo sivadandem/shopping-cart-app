38c6f433ceeea37537a7b0db22519d98fff92427
sivadandem <sivadandem7@gmail.com>
2026-02-07 12:06:50 +0530

    create User.js and write User schema

---
+++ backend/models/User.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, 'Username is required'],
+        unique: true,
+        trim: true,           
+        minlength: [3, 'Username must be at least 3 characters']
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        minlength: [6, 'Password must be at least 6 characters']
+    },
+    token: {
+        type: String,
+        default: null       
+    }
+}, {
+    timestamps: true        
+});
+
+userSchema.pre('save', async function(next) {
+   
+    if (!this.isModified('password')) {
+        return next();
+    }
+    
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+
+userSchema.methods.comparePassword = async function(candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
+
+userSchema.methods.toJSON = function() {
+    const user = this.toObject();
+    delete user.password;  
+    delete user.token;     
+    return user;
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
